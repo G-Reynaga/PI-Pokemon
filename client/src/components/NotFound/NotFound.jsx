f6fb7913a97e59af2385b4b849185cc01f6ba857
Gerson Moises Reyaga Sotelo <gerson.reynaga23@gmail.com>
2023-05-19 20:07:58 -0500

    Actualizacion

--- client/src/components/NotFound/NotFound.jsx
+++ client/src/components/NotFound/NotFound.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import e404 from "../../img/Error.png";
+import e404 from "../../img/404-Page-Not-Found.png";
 import styles from "./NotFound.module.css";
 
 const NotFound = () => {
@@ -7,15 +7,16 @@ const NotFound = () => {
   const handleNavigate = () => navigate("/");
 
   return (
-    <div className="animate__animated animate__bounceIn">
     <div className={styles.container}>
       <div className={styles.error}>
         <h1>404</h1>
-        <img src={e404} alt="Error" />
-        <h3>Sorry! the page you're looking for is not here</h3>
+        <img
+          className="animate__animated animate__tada"
+          src={e404}
+          alt="Error"
+        />
         <button onClick={handleNavigate}>Click here to return</button>
       </div>
-    </div>
     </div>
   );
 };
